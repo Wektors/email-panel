101454b5d5c42e1529efc0c4e1c8bea9c6d3ffa5
Wektors <wiktorm16@gmail.com>
2024-01-14 14:45:48 +0100

    add class components, props decorator

---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		Hi there
+	</div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component({
+	components: {
+		
+	},
+})
+export default class Main extends Vue {
+	data() {
+		return {
+		};
+	
+}
+}
+</script>
+
+<style scoped>
+
+</style>
